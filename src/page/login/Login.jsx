af68ab6ba5238fe818dccab6956c27945456da72
13614477385@163.com <>
2019-01-09 17:54:24 +0800

    登录，注册页面完善

--- src/page/login/Login.jsx
+++ src/page/login/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Form, Input, Button, message, Row, Col } from 'antd';
-import { Link } from 'react-router-dom';
+import { Form, Input, Button, message, Row, Col, Icon } from 'antd';
 import './Login.less';
 import { isAuthenticated, setCurrentLoginUser } from '../../component/Container';
 
@@ -9,6 +8,13 @@ const FormItem = Form.Item;
 @Form.create()
 export default class Login extends React.Component {
     // static getDerivedStateFromProps(){}
+
+    state = {
+        status: "Log in",
+        register: "register now!",
+        forgotPassword: 'Forgot password',
+        msgPassword: "Password"
+    };
 
     handleSubmit = (e) => {
         e.preventDefault();
@@ -19,7 +25,9 @@ export default class Login extends React.Component {
                     username,
                     password
                 };
-                setCurrentLoginUser(res);
+                if(this.state.status === "Log in"){
+                    setCurrentLoginUser(res)
+                }
                 if (username === '123' && password === '123') {
                     // 表单的路由处理
                     if(isAuthenticated()) {
@@ -37,34 +45,98 @@ export default class Login extends React.Component {
 
     };
 
+    handelRegister = () => {
+        this.props.form.resetFields();
+        const { register } = this.state;
+        if(register === "register now!"){
+            this.setState({status:"register", register:"Log in", msgPassword:"Password"})
+        }else{
+            this.setState({status:"Log in", register:"register now!", msgPassword:"Password", forgotPassword: 'Forgot password'})
+        }
+
+    };
+
+    handelPassword = () => {
+        this.props.form.resetFields();
+        this.setState({status:"reset password", register:"Log in", forgotPassword: "", msgPassword:"newPassword"})
+    };
+
+    regType = (rule, value, callback) => {
+        /* eslint-disable */
+        const reg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+        /* eslint-enable */
+        if (value && value !== '') {
+            if (rule.field === "email") {  //身份证号码
+                if (!reg.test(value)) {
+                    callback('邮箱格式不正确!');
+                    return;
+                }
+            }
+        }
+        callback();
+    };
+
     render() {
         const { form } = this.props;
         const { getFieldDecorator } = form;
+        const { status, register, forgotPassword, msgPassword } = this.state;
         return (
                 <div className="login">
+                    {/*<img src={require("../../images/a19ee33c5a26acf618411a4c96224837.jpg")} />*/}
                     <div className="box">
                         <p>小仙女才能登陆的系统</p>
+                        <br/>
                         <div className="loginWrap">
-                            <Form onSubmit={this.handleSubmit}>
+                            <Form className="login-form" onSubmit={this.handleSubmit}>
                                 <FormItem>
                                     {getFieldDecorator('username', {
-                                        rules: [{ required: true, message: '请输入用户名' }],
+                                        rules: [{ required: true, message: '请输入用户名!' }],
                                     })(
-                                        <Input placeholder="Username：123" />
+                                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                               placeholder="Username"
+                                        />
                                     )}
                                 </FormItem>
                                 <FormItem>
                                     {getFieldDecorator('password', {
-                                        rules: [{ required: true, message: '请输入密码' }],
+                                        rules: [{ required: true, message: status === "reset password"?"请输入新密码!":"请输入密码!" }],
                                     })(
-                                        <Input type="password" placeholder="Password：123" />,
+                                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                               type="password" placeholder={msgPassword}
+                                        />,
                                     )}
                                 </FormItem>
-                                <Button type="primary" htmlType="submit" className="loginBtn">Login</Button>
-                                <Link to={"#"}>register</Link>
-                                <Row style={{marginTop: 20, marginLeft: 15, color:"#aaaaaa"}}>
-                                    <Col span={14}>username:123 </Col>
-                                    <Col span={9}>password:123 </Col>
+                                {
+                                    status === "Log in" ? null :
+                                        <FormItem>
+                                            {getFieldDecorator('email', {
+                                                rules: [{ required: true, message: '请输入邮箱!' },
+                                                    { validator: this.regType}
+                                                ],
+                                            })(
+                                                <Input prefix={<Icon type="mail" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                                       type="email" placeholder="Email"
+                                                />,
+                                            )}
+                                        </FormItem>
+                                }
+                                <Row>
+                                    <Col span={12} style={{textAlign: "left"}}>
+                                        <a onClick={this.handelPassword}>{forgotPassword}</a>
+                                    </Col>
+                                    <Col span={12} style={{textAlign: "right"}}>
+                                        <a onClick={this.handelRegister}>{register}</a>
+                                    </Col>
+                                </Row>
+                                <br/>
+                                <Row>
+                                    <Button type="primary"
+                                            htmlType="submit"
+                                            className="login-form-button"
+                                            style={{width: "100%"}}
+                                    >
+                                        {status}
+                                    </Button>
                                 </Row>
                             </Form>
                         </div>
